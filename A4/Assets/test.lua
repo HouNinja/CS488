e92c746e8df159ea05a065e8c74cfdf0a9b6a3e0
cs488 <ningzhi4hou@gmail.com>
2022-11-11 16:22:21 -0500

    all done

--- A4/Assets/test.lua
+++ A4/Assets/test.lua
@@ -8,12 +8,12 @@ mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
 scene_root = gr.node('root')
 
 -- A small stellated dodecahedron.
-s3 = gr.nh_sphere('s3', {-110.3, 200, -89.4673}, 110.301)
-scene_root:add_child(s3)
-s3:set_material(mat3)
---steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
---steldodec:set_material(mat3)
---scene_root:add_child(steldodec)
+--s3 = gr.nh_sphere('s3', {-110.3, 200, -89.4673}, 110.301)
+--scene_root:add_child(s3)
+--s3:set_material(mat3)
+steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
+steldodec:set_material(mat3)
+scene_root:add_child(steldodec)
 
 white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
 magenta_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
